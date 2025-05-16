167f690b661a85f103a2f59af669b1a018c5160b
Akshay Savaliya <akshaysavaliya100@gmail.com>
2025-05-16 20:58:56 +0530

    Integrate ToDoListPage and ToDoViewModel into MainActivity
    This commit updates `MainActivity` to display the `ToDoListPage` Composable.
    
    - An instance of `ToDoViewModel` is created within `onCreate`.
    - `ToDoListPage` is now the main content of the `Scaffold`, replacing the previous `Greeting` Composable.
    - The `Greeting` Composable and its preview `GreetingPreview` have been removed.
    - The `onCreate` method is now annotated with `@RequiresApi(Build.VERSION_CODES.O)` because `ToDoViewModel` (and by extension `ToDoManager`) requires API level O for date/time operations.

--- app/src/main/java/com/akshay/todo/MainActivity.kt
+++ app/src/main/java/com/akshay/todo/MainActivity.kt
@@ -1,47 +1,29 @@
 package com.akshay.todo
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.akshay.todo.ui.theme.ToDoTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val viewModel = ToDoViewModel()
         setContent {
             ToDoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ToDoListPage(viewModel = viewModel, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ToDoTheme {
-        Greeting("Android")
     }
 }
